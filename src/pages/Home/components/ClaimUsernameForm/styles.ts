3b67c501a0b08a042e65b8f054d5382a3cfd89fe
Leandro Rodrigues <leandro.rodrigues00@hotmail.com>
2023-02-13 12:49:17 -0300

    feat: validação com zod

--- src/pages/Home/components/ClaimUsernameForm/styles.ts
+++ src/pages/Home/components/ClaimUsernameForm/styles.ts
@@ -1,4 +1,4 @@
-import { Box, styled } from '@ignite-ui/react'
+import { Box, styled, Text } from '@ignite-ui/react'
 
 export const Form = styled(Box, {
   display: 'grid',
@@ -9,5 +9,24 @@ export const Form = styled(Box, {
 
   '@media (max-width: 600px)': {
     gridTemplateColumns: '1fr',
+  },
+})
+
+export const FormAnnotation = styled('div', {
+  marginTop: '$2',
+  marginLeft: '10px',
+
+  [`> ${Text}`]: {
+    color: '$gray400',
+  },
+
+  variants: {
+    hasError: {
+      true: {
+        [`> ${Text}`]: {
+          color: 'red',
+        },
+      },
+    },
   },
 })
