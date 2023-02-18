6d9ab32a8a8476a12a10fb6ab8079855396a1b38
Leandro Rodrigues <leandro.rodrigues00@hotmail.com>
2023-02-18 09:53:00 -0300

    feat: pagina de confirmação

--- src/pages/schedule/[username]/ScheduleForm/index.tsx
+++ src/pages/schedule/[username]/ScheduleForm/index.tsx
@@ -1,9 +1,6 @@
-import { CalendarStep } from './CalendarStep'
+// import { CalendarStep } from './CalendarStep'
+import { ConfirmStep } from './ConfirmStep'
 
 export function ScheduleForm() {
-  return (
-    <>
-      <CalendarStep />
-    </>
-  )
+  return <ConfirmStep />
 }
--- src/pages/schedule/[username]/index.page.tsx
+++ src/pages/schedule/[username]/index.page.tsx
@@ -2,6 +2,7 @@ import { prisma } from '@/lib/prisma'
 import { Avatar, Heading, Text } from '@ignite-ui/react'
 import { GetStaticPaths, GetStaticProps } from 'next'
 import { ScheduleForm } from './ScheduleForm'
+
 import { Container, UserHeader } from './styles'
 
 interface ScheduleProps {
