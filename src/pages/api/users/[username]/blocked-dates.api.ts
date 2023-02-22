fd1f7c2541a674e66ccc2ff161684720e35ebb31
Leandro Rodrigues <leandro.rodrigues00@hotmail.com>
2023-02-22 11:31:33 -0300

    fix: Nenhum dia disponivel para agendamento

--- src/pages/api/users/[username]/blocked-dates.api.ts
+++ src/pages/api/users/[username]/blocked-dates.api.ts
@@ -44,24 +44,19 @@ export default async function handle(
   })
 
   const blockedDatesRaw: Array<{ date: number }> = await prisma.$queryRaw`
-    SELECT 
-      EXTRACT(DAY FROM S.date) AS date, 
+    SELECT
+      EXTRACT(DAY FROM S.DATE) AS date,
       COUNT(S.date) AS amount,
-      ((UTI.time_end_in_minutes - UTI.time_start_in_minutes)/60) AS size
-
+      ((UTI.time_end_in_minutes - UTI.time_start_in_minutes) / 60) AS size
     FROM schedulings S
 
     LEFT JOIN user_time_intervals UTI
-      on UTI.week_day = WEEKDAY(DATE_ADD(S.date, INTERVAL 1 DAY))
-    
-
+      ON UTI.week_day = WEEKDAY(DATE_ADD(S.date, INTERVAL 1 DAY))
     WHERE S.user_id = ${user.id}
-      AND DATE_FORMAT(S.date, '%Y-%m') = ${`${year}-${month}`}
-
-    GROUP BY EXTRACT(DAY FROM S.date),
-    ((UTI.time_end_in_minutes - UTI.time_start_in_minutes)/60) 
-
-    HAVING amount >= size 
+      AND DATE_FORMAT(S.date, "%Y-%m") = ${`${year}-${month}`}
+    GROUP BY EXTRACT(DAY FROM S.DATE),
+      ((UTI.time_end_in_minutes - UTI.time_start_in_minutes) / 60)
+    HAVING amount >= size
   `
 
   const blockedDates = blockedDatesRaw.map((item) => item.date)
